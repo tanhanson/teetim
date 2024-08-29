3c5b756233ca570980589c6395e48e36b13f42ac
Tan <e6194865@cmaisonneuve.qc.ca>
2024-08-29 11:46:21 -0400

    Modularisation du site complétée

---
+++ parties-communes/pied2page.php
@@ -0,0 +1,34 @@
+<footer>
+            <h2>teeTIM</h2>
+            <div class="contenu">
+                <section class="achats">
+                    <h3>Vos achats</h3>
+                    <nav>
+                        <a href="faq.com" class="faq">Foire aux questions</a>
+                        <a href="livraison.php" class="livraison">Livraison de votre colis</a>
+                        <a href="conditions.php" class="conditions">Conditions de vente</a>
+                        <a href="confidentialite.php" class="confidentialite">Politique de confidentialité</a>
+                    </nav>
+                </section>
+                <section class="apropos">
+                    <h3>À propos de teeTIM</h3>
+                    <nav>
+                        <a href="compagnie.com" class="faq">La compagnie</a>
+                        <a href="equipe.php" class="livraison">L'équipe</a>
+                        <a href="emploi.php" class="conditions">Emplois</a>
+                    </nav>
+                </section>
+                <section class="coordonnees">
+                    <h3>Nous joindre</h3>
+                    <nav>
+                        <span>Sans frais : <b>1 866 888 6666</b></span>
+                        <span>Courriel : aide@teetim.ca</span>
+                    </nav>
+                </section>
+            </div>
+            <p class="da">&copy; Tous droits réservés, teeTIM <?php echo date ("Y") ?></p>
+        </footer>
+    </div>
+    
+</body>
+</html>
--- teeshirts.php
+++ teeshirts.php
@@ -1,81 +1,17 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500;900&family=Noto+Serif:ital,wght@0,400;0,900;1,400&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>teeTIM // fibre naturelle ... conception artificielle</title>
-    <meta name="description" content="Page d'accueil du concepteur de vêtements 100% fait au Québec, conçus par les étudiants du TIM à l'aide de designs produits par intelligence artificielle, et fabriqués avec des fibres 100% naturelles et biologiques.">
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="icon" type="image/png" href="images/favicon.png" />
-</head>
-<body>
-    <div class="conteneur">
-        <header>
-            <nav class="barre-haut">
-                <a href="#">en</a>
-                <a href="#">es</a>
-                <a class="actif" href="#">fr</a>
-            </nav>
-            <nav class="barre-logo">
-                <label for="cc-btn-responsive" class="material-icons burger">menu</label>
-                <a class="logo" href="index.php"><img src="images/logo.png" alt="Accueil"></a>
-                <a class="material-icons panier" href="panier.php">shopping_cart</a>
-                <input class="recherche" type="search" name="motscles" placeholder="Recherche">
-            </nav>
-            <input type="checkbox" id="cc-btn-responsive">
-            <nav class="principale">
-                <label for="cc-btn-responsive" class="menu-controle material-icons">close</label>
-                <a href="teeshirts.php" class="actif">Teeshirts</a>
-                <a href="casquettes.php">Casquettes</a>
-                <a href="hoodies.php">Hoodies</a>
-                <span class="separateur"></span>
-                <a href="aide.php">Aide</a>
-                <a href="apropos.php">À propos de nous</a>
-            </nav>
-        </header>
-        <main class="page-teeshirts">
-            <article class="amorce">
-                <h1>Nos teeshirts</h1>
-            </article>
-            <article class="principal">
-                À venir...
-            </article>
-        </main>
-        <footer>
-            <h2>teeTIM</h2>
-            <div class="contenu">
-                <section class="achats">
-                    <h3>Vos achats</h3>
-                    <nav>
-                        <a href="faq.com" class="faq">Foire aux questions</a>
-                        <a href="livraison.php" class="livraison">Livraison de votre colis</a>
-                        <a href="conditions.php" class="conditions">Conditions de vente</a>
-                        <a href="confidentialite.php" class="confidentialite">Politique de confidentialité</a>
-                    </nav>
-                </section>
-                <section class="apropos">
-                    <h3>À propos de teeTIM</h3>
-                    <nav>
-                        <a href="compagnie.com" class="faq">La compagnie</a>
-                        <a href="equipe.php" class="livraison">L'équipe</a>
-                        <a href="emploi.php" class="conditions">Emplois</a>
-                    </nav>
-                </section>
-                <section class="coordonnees">
-                    <h3>Nous joindre</h3>
-                    <nav>
-                        <span>Sans frais : <b>1 866 888 6666</b></span>
-                        <span>Courriel : aide@teetim.ca</span>
-                    </nav>
-                </section>
-            </div>
-            <p class="da">&copy; Tous droits réservés, teeTIM <?php echo date("Y") ?></p>
-        </footer>
-    </div>
-    
-</body>
-</html>
+<?php 
+	// Créer une variable pour indiquer le "nom" de la page
+	$page = "teeshirts";
+
+	// Inclure le code du fichier commun qui contient tout le haut de la page
+	include("parties-communes/entete.php"); 
+	
+?>
+<main class="page-teeshirts">
+    <article class="amorce">
+        <h1>Nos teeshirts</h1>
+    </article>
+    <article class="principal">
+        À venir...
+    </article>
+</main>
+<?php include("parties-communes/pied2page.php"); ?>
